06a0d9011a073d59747337ec28712ef2433a15d2
unknown <chathree.com>
2022-07-09 02:32:08 -0600

    operators

---
+++ python2.py
@@ -0,0 +1,87 @@
+'''
+operators
+are sumbols or in amaharic word hulet negerochin yemiyagenagnln neger new
+and opreators are classified in to 7 some of them are
+-arithmetic
+-assignment
+-comparison
+-logical
+-identity
+-membership
+-bitwise
+'''
+
+
+#arithmetic
+from ast import Not, operator
+from turtle import width
+
+
+print('operators\t\tname\t\t\texample')
+print('+\t\t\tadition\t\t\tx + y\n\n-\t\t\tsub\t\t\tx - y\n\n*\t\t\tmult\t\t\tx * y\n\n/\t\t\tdivi\t\t\tx / y\n\n%\t\t\tmodulus\t\t\tx % y\n\n**\t\t\texponentistion\t\tx ** y\n\n//\t\t\tfloor divi\t\tx // y')
+num1=34
+num2=34.5
+z1=num1+num2 #num1 and num2 are operands
+z2=num1*num2 
+z3=num1/num2
+z4=num1%num2
+z5=num1**num2
+z6=num1//num2
+print('adding num1 and numz the soln is this',z1)
+print('multiplaying num1 and numz the soln is this',z2)
+print('dividning num1 and numz the soln is this',z3)
+print('mojulo or in amaharic keri num1 and numz the soln is this',z4)
+print('power of num1 and numz the soln is this',z5)
+print('with out remender it divied num1 and numz the soln is this',z6)
+
+
+#assignment operators are (+,-,/,%,**,//)
+num1+=23
+num2*=23
+num1/=23
+num1%=23
+num1**=23
+num1//=23
+area=num1+num2
+width=num1+num2
+print(width)
+print('the area of the rectangle',area)
+print('adding 23 in num1',num1)
+print('multiplaying 23 in num2',num2)
+print('dividing 23 in num1',num1)
+print('remending 23 in num1',num1)
+print('powering 23 in num1',num1)
+print('wit out remender dividing 23 in num1',num1)
+
+
+#comaparison operators are (==this is eqaual sign,)
+#                          (!= not equal sign)
+#                          (> greter than sign)
+#                          (< less than)
+#                          (>= greter or eqaul to)
+#                          (<= less than or eqal to)
+#let me show an example
+num1=23
+num=24
+print(num1==num2)
+print(num1!=num2)
+print(num1>num2)
+print(num1<num2)
+print(num1>=num2)
+print(num1<=num2)
+
+
+#logical operator are (and or not)
+#let me see and example
+name1=23
+name2=24
+print(name1 >= name2 or name1<=name2)
+print(name1==name2 and name1<name2)
+name3='chalew is small'
+name4='shamero is bigg'
+print(name3==name4)
+
+
+
+
+
